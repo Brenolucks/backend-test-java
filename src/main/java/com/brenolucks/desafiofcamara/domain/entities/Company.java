ed1e15c717889dc4d45207b432e75bcbb0f6e67f
brenolucks <brenolucas0106@gmail.com>
2024-02-20 00:12:21 -0300

    Add atributes in entity classes

--- src/main/java/com/brenolucks/desafiofcamara/domain/entities/Company.java
+++ src/main/java/com/brenolucks/desafiofcamara/domain/entities/Company.java
@@ -1,4 +1,21 @@
 package com.brenolucks.desafiofcamara.domain.entities;
 
-public class Company {
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "tb_company")
+public class Company implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String cnpj;
+    private String address;
+    private int phone;
+    private int amountVacancyMoto;
+    private int amountVacancyCar;
 }
--- src/main/java/com/brenolucks/desafiofcamara/domain/entities/Vehicle.java
+++ src/main/java/com/brenolucks/desafiofcamara/domain/entities/Vehicle.java
@@ -1,4 +1,20 @@
 package com.brenolucks.desafiofcamara.domain.entities;
 
-public class Vehicle {
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "tb_vehicle")
+public class Vehicle implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String brand;
+    private String model;
+    private String color;
+    private String plate;
+    private String type;
 }
